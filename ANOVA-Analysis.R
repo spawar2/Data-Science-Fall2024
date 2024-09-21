787e09eb05a7f031eecd3e5e9ce9d68c2548d0d3
Pawar <pawar1550@gmail.com>
2024-09-21 07:43:32 -0400

    Add files via upload

---
+++ ANOVA-Analysis.R
@@ -0,0 +1,18 @@
+# Author: Pawar, Date: September 20, 2024, Purpose: To perform ANOVA test
+
+# Hypothesis: Treatment A is most effective on poison type 1. Expecting significant differences in treatment and poison. This is my alternate hypothesis.
+# Null hypothesis: There is not significant differences in treatment type A and other treatment types.
+
+# Where can your input data be stored?
+# Local drive (external drive/USB, hardrive, server, cluster); Cloud storage (amazon cloud, google cloud, Apple icloud); HTML page (GitHub public repository).
+
+# Import the dummy data from GitHub public repository in our R program
+
+library(dplyr)
+
+
+
+
+
+
+
