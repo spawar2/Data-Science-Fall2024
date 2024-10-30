f0c2e130b23b182ae968d060d1cf8ab3725007c4
Pawar <pawar1550@gmail.com>
2024-10-30 13:24:29 -0400

    Add files via upload

--- Ttest-Normal-Distribution-Plot.R
+++ Ttest-Normal-Distribution-Plot.R
@@ -8,12 +8,23 @@ y = rnorm(100)
 pts = seq(-4.5,4.5,length=100)
 
 # Create a density plot for 100 points between -4.5 and +4.5
-plot(pts,dt(pts,df=9),col='red',type='l')
+plot(pts,dt(pts,df=9),col='red',type='l', xlab="Points", ylab="Density")
 
 # X axis label "Points", and Y axis label "Density"
-
 
 lines(density(x), col='green')
 lines(density(y), col='blue')
 ttest = t.test(x,y)
 ttest
+
+
+	Welch Two Sample t-test
+
+data:  x and y
+t = 1.0336, df = 102.51, p-value = 0.3037
+alternative hypothesis: true difference in means is not equal to 0
+95 percent confidence interval:
+ -0.1717994  0.5457389
+sample estimates:
+  mean of x   mean of y 
+ 0.14164879 -0.04532093 
